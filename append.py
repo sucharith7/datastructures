38690d0c5541a60c2f90c4f12bf58b27d6f43763
sucharith7 <ch.sucharith09@gmail.com>
2020-04-10 23:46:36 +0530

    [sucharith] added append file

---
+++ append.py
@@ -0,0 +1,31 @@
+class Node():
+    
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+
+class Linked_list():
+
+    def __init__(self):
+        self.head=None
+
+
+
+    def print_list(self):
+    	cur_node=self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node=cur_node.next
+
+
+
+    def append(self, data):
+        new_node=Node(data)
+        if self.head is None:
+            self.head=new_node
+            return None
+        last_node=self.head
+        while last_node.next:
+            last_node=last_node.next
+        last_node.next=new_node
